97dd199d7bb5e721b726f1020cf99a29d479c89b
Atharva Lotankar <atharvalotankar11@gmail.com>
2025-09-28 21:06:49 +0530

    OfficePulse_Website

--- reset_git.ps1
+++
@@ -1,9 +0,0 @@
-Stop-Process -Name "git" -Force -ErrorAction SilentlyContinue
-Stop-Process -Name "less" -Force -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 2
-Remove-Item -Recurse -Force .git -ErrorAction SilentlyContinue
-git init
-git remote add origin https://github.com/AtharvaLotankar11/OfficePulse_Project.git
-git add .
-git commit -m "Initial commit - OfficePulse project"
-git push -u origin main
